cd40290ffdf2b5218bf6c4f51083d578f01eb2ad
lamans000 <ansonlam640@gmail.com>
2019-09-13 14:56:09 -0400

    updated age and front page

---
+++ public/jsonPractice.php
@@ -0,0 +1,22 @@
+[
+{ "patientGuid": "001",
+  "firstName": "John",
+  "lastName": "Doe",
+  "dob": "2012-09-01",
+  "sexAtbirth": "M"
+},
+{
+  "patientGuid": "002",
+    "firstName": "Jane",
+    "lastName": "Doe",
+    "dob": "1986-05-20",
+    "sexAtbirth": "F"
+},
+{
+  "patientGuid": "003",
+    "firstName": "Johnny",
+    "lastName": "Appleseed",
+    "dob": "1966-04-25",
+    "sexAtbirth": "M"
+}
+]
